3880dbe74650e1c28537e3694e720cb85bd0575a
tob <tobiadonati@gmail.com>
2017-08-15 17:02:06 +0200

    adding redux store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.sass';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import { Provider } from 'react-redux'
+import store from './store'
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
