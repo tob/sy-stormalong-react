2dbcefe40fed6d0e65b93cbfc5e813d95bf6aa60
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-08-17 18:52:32 +0200

    fix html render edit color palette and add home button in nav

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -2,25 +2,25 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
 // Colors
-export const green        = '#00AA86'
+export const otherblue    = '#4E8098'
 export const red          = '#D32F2F'
-export const darkRed      = '#C1272D'
-export const white        = '#ffffff'
-export const black        = '#000000'
+export const blue         = '#4D6A78'
+export const lightblue    = '#90C2E7'
+export const white        = '#FCF7F8'
 export const darkGrey     = '#757575'
-export const grey         = '#DEDEDE'
+export const grey         = '#CED3DC'
 export const grey50       = 'rgba(222, 222, 222, 0.5)'
 export const grey30       = 'rgba(222, 222, 222, 0.7)'
 
 // Palette
 export const palette = {
-  primary1Color: red,
-  primary2Color: green,
-  primary3Color: green,
-  accent1Color: green,
-  textColor: black,
+  primary1Color: blue,
+  primary2Color: lightblue,
+  primary3Color: lightblue,
+  accent1Color: otherblue,
+  textColor: blue,
   alternateTextColor: white,
-  canvasColor: white,
+  canvasColor: lightblue,
   borderColor: grey,
   disabledColor: grey30
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,14 +2,19 @@ import React, { PureComponent } from 'react'
 import AppBar from 'material-ui/AppBar'
 import IconButton from 'material-ui/IconButton'
 import MyLocation from 'material-ui/svg-icons/maps/my-location'
+import { history } from '../store'
 
 class Navigation extends PureComponent {
+
+  goHome() {
+    history.push('/')
+  }
 
   render() {
     return (
       <AppBar
         title="Stormalong"
-        iconElementLeft={<IconButton><MyLocation /></IconButton>}/>
+        iconElementLeft={<IconButton onClick={ this.goHome }><MyLocation /></IconButton>}/>
     )
   }
 
